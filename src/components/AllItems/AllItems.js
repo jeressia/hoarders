ed5030ab71ebb99f6f68870a420874e647687378
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-07-05 10:08:12 -0500

    setup done

---
+++ src/components/AllItems/AllItems.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './AllItems.scss';
+
+class AllItems extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <AllItems />
+      </div>
+    );
+  }
+}
+
+export default AllItems;
